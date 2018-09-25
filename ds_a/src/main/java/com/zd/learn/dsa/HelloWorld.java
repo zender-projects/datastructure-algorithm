b0d893afd682aa1aeaf2d2074fec6c9dbb418366
ivan <15101535064@163.com>
2018-09-26 07:57:09 +0800

    add java file to test

---
+++ ds_a/src/main/java/com/zd/learn/dsa/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.zd.learn.dsa;
+
+public class HelloWorld {
+    public static void main(String args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ ds_a/src/test/java/com/zd/learn/dsa/test/Tester.java
@@ -0,0 +1,11 @@
+package com.zd.learn.dsa.test;
+
+import org.junit.Test;
+
+public class Tester {
+
+    @Test
+    public void helloWorld() {
+        System.out.println("Hello World!");
+    }
+}
